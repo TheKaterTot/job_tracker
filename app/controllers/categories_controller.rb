1f1955aa532c810a82f8bfa63f5b7351e3db264d
Katy Farmer <dreadpiratekaty@gmail.com>
2017-02-09 22:22:10 -0700

    Adds update for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :destroy]
+  before_action :set_category, only: [:show, :destroy, :edit, :update]
 
   def index
     @categories = Category.all
@@ -21,14 +21,24 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update_attributes(category_params)
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @category.destroy
     redirect_to categories_path
   end
 
-
-
   private
+
   def category_params
     params.require(:category).permit(:title)
   end
--- spec/features/categories/user_updates_a_category_spec.rb
+++ spec/features/categories/user_updates_a_category_spec.rb
@@ -1,1 +1,18 @@
 require "rails_helper"
+
+feature "User updates a category" do
+  let!(:category) { Fabricate(:category) }
+  context "with valid info" do
+    scenario "they visit edit" do
+      visit edit_category_path(category)
+
+      fill_in "category[title]", with: "Tech"
+
+      click_button "Update"
+
+      category.reload
+      expect(current_path).to eq(category_path(category))
+      expect(page).to have_content("Tech")
+    end
+  end
+end
